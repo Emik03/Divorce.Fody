92b209236d853fcc106c5b61ad13ef96a788075b
Emik <emik.lakunishok@gmail.com>
2023-01-18 05:06:30 +0100

    Use SPDX short-form identifiers

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -1,13 +1,4 @@
-#region Emik.MPL
-
-// <copyright file="GlobalUsings.cs" company="Emik">
-// Copyright (c) Emik. This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
-// </copyright>
-
-#endregion
-
-#region
-
+// SPDX-License-Identifier: MPL-2.0
 #if NET6_0
 global using Divorce.Fody;
 #elif NETSTANDARD2_0
@@ -19,5 +10,3 @@ global using Mono.Cecil;
 global using Mono.Cecil.Rocks;
 global using IMonoProvider = Mono.Cecil.ICustomAttributeProvider;
 #endif
-
-#endregion
