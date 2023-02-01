66e89d7de8b88112585f9dfb2e608dd406a291de
Emik <emik.lakunishok@gmail.com>
2023-02-01 20:30:55 +0100

    Wrap

--- Source/Program.cs
+++ Source/Program.cs
@@ -1,15 +1,13 @@
 ﻿// SPDX-License-Identifier: MPL-2.0
-#if !NETSTANDARD2_0
-// ReSharper disable WrongIndentSize
-var standardOutput =
-    Func(Console.ReadLine)
-       .Forever()
-       .Select(Invoke)
-       .TakeUntil(string.IsNullOrWhiteSpace);
+#if !NETSTANDARD2_0 // ReSharper disable WrongIndentSize
+var standardOutput = Func(Console.ReadLine)
+   .Forever()
+   .Select(Invoke)
+   .TakeUntil(string.IsNullOrWhiteSpace);
 
 args
-   .DefaultIfEmpty(standardOutput)
    .Where(File.Exists)
+   .DefaultIfEmpty(standardOutput)
    .Select(AssemblyDefinition.ReadAssembly)
    .Filter()
    .Select(Bye.Generics)
